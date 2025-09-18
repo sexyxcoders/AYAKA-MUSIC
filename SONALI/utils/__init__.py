c1a2d82baa1ff168eb3240afc675a7908d73857d
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-18 23:28:09 +0530

    Update __init__.py

--- SONALI/utils/__init__.py
+++ SONALI/utils/__init__.py
@@ -1,5 +1,3 @@
-# SONALI/utils/__init__.py
-
 from .channelplay import *
 from .database import *
 from .decorators import *
@@ -8,9 +6,3 @@ from .formatters import *
 from .inline import *
 from .pastebin import *
 from .sys import *
-
-# Import clone manager (fixed path)
-from .clone_manager import start_clone, stop_clone
-
-# Make config accessible project-wide
-import config
