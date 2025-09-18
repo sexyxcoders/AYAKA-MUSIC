60b2ed90b523edcb69c8de12f55cbdbad8a78516
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-18 16:24:00 +0530

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -1,7 +1,7 @@
 import asyncio 
 import time
 import random
-import config
+from config import LOGGER_ID, BANNED_USERS
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardMarkup, Message
