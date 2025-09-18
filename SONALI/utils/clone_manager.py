304eac10626021fbc93e94f6727a65394c79c53e
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-18 22:55:46 +0530

    Create clone_manager.py

---
+++ SONALI/utils/clone_manager.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from SONALI import app
+import config
+from SONALI.clone_manager import start_clone, stop_clone
+
+
+@app.on_message(filters.command("clone") & filters.user(config.OWNER_ID))
+async def clone_handler(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Usage: /clone <BOT_TOKEN>")
+
+    token = message.command[1]
+    msg = await start_clone(token, config.API_ID, config.API_HASH)
+    await message.reply_text(msg)
+
+
+@app.on_message(filters.command("stopclone") & filters.user(config.OWNER_ID))
+async def stopclone_handler(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Usage: /stopclone <BOT_TOKEN>")
+
+    token = message.command[1]
+    msg = await stop_clone(token)
+    await message.reply_text(msg)
+  
